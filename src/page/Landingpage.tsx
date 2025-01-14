4813aa6880f356d4e64eea73f88723d956ebfb74
esckles <davidsamzzywilliams1234@gmail.com>
2025-01-14 12:59:30 +0100

    new

--- src/page/Landingpage.tsx
+++ src/page/Landingpage.tsx
@@ -4,22 +4,24 @@ const Landingpage = () => {
   return (
     <div>
       <div className="w-full h-screen">
-        <div className="w-full h-[80px] bg-slate-300 flex items-center justify-center">
-          <div className="w-[90%] h-[100%] flex justify-between items-center ">
+        <div className="w-full min-h-[80px] bg-slate-300 items-center justify-center flex ">
+          <div className="w-[90%] h-[100%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  items-center justify-between ">
             <p className="text-blue-400 font-semibold text-[30px]">
               e-sam Store
             </p>
-            <input
-              type="text"
-              placeholder="Search..."
-              className="outline-none border w-[400px] py-2 rounded-md p-2"
-            />
-            <button className="px-4 py-3 rounded-md flex items-center justify-center text-white font-semibold bg-blue-600">
-              <Link to="/auth/signup">Signup</Link>
-            </button>
+            <div className="flex justify-between items-center ">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="outline-none border w-[400px] h-[40px] rounded-md p-2"
+              />
+              <button className="w-[100px] h-[50px] rounded-md flex items-center justify-center text-white font-semibold bg-blue-600">
+                <Link to="/auth/signup">Signup</Link>
+              </button>
+            </div>
           </div>
         </div>
-        <div className=" w-full h-[calc(100vh-80px)] flex items-center justify-center text-[70px] font-semibold text-blue-400 ">
+        <div className=" w-full h-[calc(100vh-80px)]  items-center justify-center text-[70px] font-semibold text-blue-400 grid md:mt-[50px] ">
           Signup up with us and checkout <br /> our latest Top notch products{" "}
           <br /> out for Sales
         </div>
