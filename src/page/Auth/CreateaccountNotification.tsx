dc995aa501140849dae03dfbdb64f240380764c0
esckles <davidsamzzywilliams1234@gmail.com>
2025-01-14 09:40:11 +0100

    new

--- src/page/Auth/CreateaccountNotification.tsx
+++ src/page/Auth/CreateaccountNotification.tsx
@@ -1,7 +1,7 @@
 const CreateaccountNotification = () => {
   return (
     <div className="flex items-center justify-center h-screen w-full font-semibold">
-      Your account has been created go to your email to verify ❤️❤️
+      Your account has been created go to your email to verify ❤️❤️❤️
     </div>
   );
 };
